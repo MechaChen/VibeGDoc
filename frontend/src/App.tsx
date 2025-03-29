e7ef2a66535ebaf66a1aa49234d03b125effd9d9
Benson Chen <tumlivein@gmail.com>
2025-03-29 22:11:36 +0800

    Style editor & add custom plugin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css'
-import {$getRoot, $getSelection} from 'lexical';
+import {$getRoot, $getSelection, EditorState} from 'lexical';
 import {useEffect} from 'react';
 
-import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
 import {LexicalComposer} from '@lexical/react/LexicalComposer';
 import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
 import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
 import {LexicalErrorBoundary} from '@lexical/react/LexicalErrorBoundary';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 
 const theme = {
   // Theme styling goes here
@@ -19,6 +19,17 @@ const theme = {
 // try to recover gracefully without losing user data.
 function onError(error: Error): void {
   console.error(error);
+}
+
+function MyOnChangePlugin(props: { onChange: (editorState: EditorState) => void }) {
+  const [editor] = useLexicalComposerContext();
+  const { onChange } = props;
+
+  useEffect(() => {
+    return editor.registerUpdateListener(({editorState}) => {
+      onChange(editorState);
+    });
+  }, [editor, onChange]);
 }
 
 function Editor() {
@@ -33,22 +44,28 @@ function Editor() {
       <RichTextPlugin
         contentEditable={
           <ContentEditable
+            className="h-[300px] w-full rounded-xl border-gray-300 border p-2"
             aria-placeholder={'Enter some text...'}
-            placeholder={<div>Enter some text...</div>}
+            placeholder={<div className="absolute top-0 left-0 text-gray-500 p-2">Enter some text...</div>}
           />
         }
         ErrorBoundary={LexicalErrorBoundary}
       />
       <HistoryPlugin />
-      <AutoFocusPlugin />
+      <MyOnChangePlugin onChange={(editorState: EditorState) => {
+        console.log(editorState);
+      }}/>
     </LexicalComposer>
   );
 }
 
 function App() {
   return (
-    <div style={{width: '100%', height: '100%'}}>
-      <Editor />
+    <div className="w-[80%] m-auto py-10">
+      <h2 className="text-2xl font-bold pb-4">Vibe Google Doc</h2>
+      <div className="relative">
+        <Editor />
+      </div>
     </div>
   )
 }
